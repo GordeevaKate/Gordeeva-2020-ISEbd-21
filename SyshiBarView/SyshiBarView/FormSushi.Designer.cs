1828ac5f8f13d4a493bd19f48096b2b8358c3b7b
maa.vaa.81@mail.ru <maa.vaa.81@mail.ru>
2020-03-19 09:44:16 +0400

    1

--- SyshiBarView/SyshiBarView/FormSushi.Designer.cs
+++ SyshiBarView/SyshiBarView/FormSushi.Designer.cs
@@ -28,14 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelPrice = new System.Windows.Forms.Label();
             this.buttonAdd = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.buttonRef = new System.Windows.Forms.Button();
+            this.buttonDel = new System.Windows.Forms.Button();
+            this.buttonUpd = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonCansel = new System.Windows.Forms.Button();
             this.textBoxName = new System.Windows.Forms.TextBox();
             this.textBoxPrice = new System.Windows.Forms.TextBox();
             this.dataGridView = new System.Windows.Forms.DataGridView();
@@ -45,23 +45,23 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // labelName
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Название:";
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(12, 9);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(60, 13);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Название:";
             // 
-            // label2
+            // labelPrice
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 42);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(36, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Цена:";
+            this.labelPrice.AutoSize = true;
+            this.labelPrice.Location = new System.Drawing.Point(12, 42);
+            this.labelPrice.Name = "labelPrice";
+            this.labelPrice.Size = new System.Drawing.Size(36, 13);
+            this.labelPrice.TabIndex = 1;
+            this.labelPrice.Text = "Цена:";
             // 
             // buttonAdd
             // 
@@ -73,55 +73,55 @@
             this.buttonAdd.UseVisualStyleBackColor = true;
             this.buttonAdd.Click += new System.EventHandler(this.ButtonAdd_Click);
             // 
-            // button2
+            // buttonRef
             // 
-            this.button2.Location = new System.Drawing.Point(441, 246);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(115, 29);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Обновить";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.ButtonRef_Click);
+            this.buttonRef.Location = new System.Drawing.Point(441, 246);
+            this.buttonRef.Name = "buttonRef";
+            this.buttonRef.Size = new System.Drawing.Size(115, 29);
+            this.buttonRef.TabIndex = 3;
+            this.buttonRef.Text = "Обновить";
+            this.buttonRef.UseVisualStyleBackColor = true;
+            this.buttonRef.Click += new System.EventHandler(this.ButtonRef_Click);
             // 
-            // button3
+            // buttonDel
             // 
-            this.button3.Location = new System.Drawing.Point(441, 197);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(115, 29);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Удалить";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.ButtonDel_Click);
+            this.buttonDel.Location = new System.Drawing.Point(441, 197);
+            this.buttonDel.Name = "buttonDel";
+            this.buttonDel.Size = new System.Drawing.Size(115, 29);
+            this.buttonDel.TabIndex = 4;
+            this.buttonDel.Text = "Удалить";
+            this.buttonDel.UseVisualStyleBackColor = true;
+            this.buttonDel.Click += new System.EventHandler(this.ButtonDel_Click);
             // 
-            // button4
+            // buttonUpd
             // 
-            this.button4.Location = new System.Drawing.Point(441, 153);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(115, 29);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "Изменить";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.ButtonUpd_Click);
+            this.buttonUpd.Location = new System.Drawing.Point(441, 153);
+            this.buttonUpd.Name = "buttonUpd";
+            this.buttonUpd.Size = new System.Drawing.Size(115, 29);
+            this.buttonUpd.TabIndex = 5;
+            this.buttonUpd.Text = "Изменить";
+            this.buttonUpd.UseVisualStyleBackColor = true;
+            this.buttonUpd.Click += new System.EventHandler(this.ButtonUpd_Click);
             // 
-            // button5
+            // buttonSave
             // 
-            this.button5.Location = new System.Drawing.Point(309, 387);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(115, 29);
-            this.button5.TabIndex = 6;
-            this.button5.Text = "Сохранить";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.ButtonSave_Click);
+            this.buttonSave.Location = new System.Drawing.Point(309, 387);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(115, 29);
+            this.buttonSave.TabIndex = 6;
+            this.buttonSave.Text = "Сохранить";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.ButtonSave_Click);
             // 
-            // button6
+            // buttonCansel
             // 
-            this.button6.Location = new System.Drawing.Point(441, 387);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(115, 29);
-            this.button6.TabIndex = 7;
-            this.button6.Text = "Отмена";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.ButtonCancel_Click);
+            this.buttonCansel.Location = new System.Drawing.Point(441, 387);
+            this.buttonCansel.Name = "buttonCansel";
+            this.buttonCansel.Size = new System.Drawing.Size(115, 29);
+            this.buttonCansel.TabIndex = 7;
+            this.buttonCansel.Text = "Отмена";
+            this.buttonCansel.UseVisualStyleBackColor = true;
+            this.buttonCansel.Click += new System.EventHandler(this.ButtonCancel_Click);
             // 
             // textBoxName
             // 
@@ -177,14 +177,14 @@
             this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.textBoxPrice);
             this.Controls.Add(this.textBoxName);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonCansel);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonUpd);
+            this.Controls.Add(this.buttonDel);
+            this.Controls.Add(this.buttonRef);
             this.Controls.Add(this.buttonAdd);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelPrice);
+            this.Controls.Add(this.labelName);
             this.Name = "FormSushi";
             this.Text = "СУШИ";
             this.Load += new System.EventHandler(this.FormProduct_Load);
@@ -196,14 +196,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelPrice;
         private System.Windows.Forms.Button buttonAdd;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button buttonRef;
+        private System.Windows.Forms.Button buttonDel;
+        private System.Windows.Forms.Button buttonUpd;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonCansel;
         private System.Windows.Forms.TextBox textBoxName;
         private System.Windows.Forms.TextBox textBoxPrice;
         private System.Windows.Forms.DataGridView dataGridView;

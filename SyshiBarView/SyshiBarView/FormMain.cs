cf194b0ddc7f6b4dac96eb215652f7b8c196245c
maa.vaa.81@mail.ru <maa.vaa.81@mail.ru>
2020-05-10 10:41:10 +0400

    1

--- SyshiBarView/SyshiBarView/FormMain.cs
+++ SyshiBarView/SyshiBarView/FormMain.cs
@@ -146,5 +146,27 @@ namespace SyshiBarView
             form.ShowDialog();
         }
 
+        private void buttonDel_Click(object sender, EventArgs e)
+        {
+            if (dataGridView.SelectedRows.Count == 1)
+            {
+                if (MessageBox.Show("Удалить запись", "Вопрос", MessageBoxButtons.YesNo,
+               MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    int id =
+                   Convert.ToInt32(dataGridView.SelectedRows[0].Cells[0].Value);
+                    try
+                    {
+                        orderLogic.Delete(new OrderBindingModel { Id = id });
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK,
+                       MessageBoxIcon.Error);
+                    }
+                    LoadData();
+                }
+            }
+        }
     }
 }

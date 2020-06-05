8f5ebf2c98ee4c75ccf8467e0f0a6403ca2f31fb
maa.vaa.81@mail.ru <maa.vaa.81@mail.ru>
2020-06-05 12:25:24 +0400

    1

--- SyshiBarView/AbstractSyshiBarBusinessLogic/ViewModels/BaseViewModel.cs
+++ SyshiBarView/AbstractSyshiBarBusinessLogic/ViewModels/BaseViewModel.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Runtime.Serialization;
-using AbstractSyshiBarBusinessLogic.Attributes
+using AbstractSyshiBarBusinessLogic.Attributes;
 namespace AbstractSyshiBarBusinessLogic.ViewModels
 {
     [DataContract]
